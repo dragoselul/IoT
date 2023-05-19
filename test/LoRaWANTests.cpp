03cfb608aabfb033c1b4f90ba85efda26afb0c1a
dragoselul <dragosbonaparte@gmail.com>
2023-05-19 11:57:31 +0200

    Update LoRaWANTests.cpp
    added all the reset functions

--- test/LoRaWANTests.cpp
+++ test/LoRaWANTests.cpp
@@ -58,17 +58,34 @@ protected:
 		RESET_FAKE(xTaskCreate);
 		RESET_FAKE(xTaskGetTickCount);
 		RESET_FAKE(xTaskDelayUntil);
-		RESET_FAKE(vTaskDelay)/*
-		RESET_FAKE(threshold_create);
-		RESET_FAKE(threshold_destroy);
-		RESET_FAKE(get_temperature_threshold);
-		RESET_FAKE(get_humidity_threshold);
-		RESET_FAKE(get_c02_threshold);
-		RESET_FAKE(get_light_threshold);
-		RESET_FAKE(set_temperature_threshold);
-		RESET_FAKE(set_humidity_threshold);
-		RESET_FAKE(set_light_threshold);
-        RESET_FAKE(set_co2_threshold); */
+		RESET_FAKE(vTaskDelay);
+		RESET_FAKE(lora_driver_getMaxPayloadSize);
+		RESET_FAKE(lora_driver_initialise);
+		RESET_FAKE(lora_driver_setOtaaIdentity);
+		RESET_FAKE(lora_driver_configureToEu868);
+		RESET_FAKE(lora_driver_mapReturnCodeToText);
+		RESET_FAKE(lora_driver_setAbpIdentity);
+		RESET_FAKE(lora_driver_join);
+		RESET_FAKE(lora_driver_sendUploadMessage);
+		RESET_FAKE(lora_driver_setDeviceIdentifier);
+		RESET_FAKE(lora_driver_setApplicationIdentifier);
+		RESET_FAKE(lora_driver_setApplicationKey);
+		RESET_FAKE(lora_driver_setNetworkSessionKey);
+		RESET_FAKE(lora_driver_setApplicationSessionKey);
+		RESET_FAKE(lora_driver_rn2483FactoryReset);
+		RESET_FAKE(lora_driver_getRn2483Hweui);
+		RESET_FAKE(lora_driver_setReceiveDelay);
+		RESET_FAKE(lora_driver_saveMac);
+		RESET_FAKE(lora_driver_setAdaptiveDataRate);
+		RESET_FAKE(lora_driver_resetRn2483);
+		RESET_FAKE(lora_driver_flushBuffers);
+		RESET_FAKE(status_leds_initialise);
+		RESET_FAKE(status_leds_longPuls);
+		RESET_FAKE(status_leds_shortPuls);
+		RESET_FAKE(status_leds_slowBlink);
+		RESET_FAKE(status_leds_fastBlink);
+		RESET_FAKE(status_leds_ledOn);
+		RESET_FAKE(status_leds_ledOff);
 		FFF_RESET_HISTORY();
 	}
 	void TearDown() override
