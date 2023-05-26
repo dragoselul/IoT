cefb1360e154f2522deab1d814748b6948406e2b
Danny <ceapadansebastian@gmail.com>
2023-05-26 11:33:01 +0200

    Added some comments for sound.h

--- src/Headers/Sound.h
+++ src/Headers/Sound.h
@@ -7,9 +7,20 @@
 
 typedef struct sound* sound_t;
 
+//Creates a sound
 sound_t sound_create();
+
+//Destroys the created sound
 void sound_destroy(sound_t self);
+
+//Gets the gate of the sound
 bool get_sound_gate_state();
+
+//Detects the sound around the sensor
 bool soundDetection(sound_t self);
+
+//Creates the sound task for measuring sound
 void create_sound_task(sound_t*);
+
+//Task implementation for the sound
 void sound_task(void*);
